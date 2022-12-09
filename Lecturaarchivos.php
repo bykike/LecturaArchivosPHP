0f4e435170f67fedbc5100ee4ecf69d88795f24b
Kike Algarra <kike@areavisual.net>
2022-12-09 17:34:04 +0100

    fread y fscanf
    fread y fscanf.

--- Lecturaarchivos.php
+++ Lecturaarchivos.php
@@ -7,7 +7,26 @@
     $fp = fopen("miarchivo.txt", "r");
     while (!feof($fp)){
         $linea = fgets($fp);
-        echo $linea;
+        echo $linea . "<br>";
+    }
+    fclose($fp);
+
+
+    # 2. fread
+
+    $fp = fopen("miarchivo.txt", "rb");
+    $datos = fread($fp, filesize("miarchivo.txt"));
+    echo $datos . "<br>";
+    fclose($fp);
+
+
+    
+    # 2. fscanf
+
+    $fp = fopen("miarchivo.txt", "r");
+    while ($animalinfo = fscanf($fp, "%s\t%s")){
+        list($animal, $nombre) = $animalinfo;
+        echo "El " . $animalinfo[0] . " se llama {$animalinfo[1]} <br>";
     }
     fclose($fp);
 
